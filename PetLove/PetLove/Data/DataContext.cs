6c98b9eecd4f643805c4dfd515aa465417575f8a
Gerardo Lanza <glanza007@gmail.com>
2022-12-29 11:45:38 -0600

    Look and Feel

---
+++ PetLove/PetLove/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetLove.Data.Entities;
+
+namespace PetLove.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+
+}
---
+++ PetLove/PetLove/Data/Entities/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetLove.Data.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "DNI")]
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Display(Name = "Dirección")]
+        public string Address { get; set; }
+
+        [Display(Name = "Cliente")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Cliente")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
